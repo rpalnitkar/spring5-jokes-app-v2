ef6a491a2e0fba67756f103075728267cb8e315e
Rahul Palnitkar <rpalnitkar@expmicrosys.com>
2023-05-01 15:18:15 -0400

    Init project using spring initilizr

---
+++ src/main/java/com/expmicrosys/spring5jokesappv2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.expmicrosys.spring5jokesappv2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Spring5JokesAppV2Application.class);
+	}
+
+}
